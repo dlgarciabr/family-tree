c38a12dcba725f6e8a92839b596fd64aa134c62a
Daniel Garcia <dlgarciabr@gmail.com>
2021-11-24 12:12:38 +0000

    improved app test

--- src/views/App/test.tsx
+++ src/views/App/test.tsx
@@ -60,3 +60,57 @@ test("Change language to Portuguese", async () => {
   //assert
   expect(await screen.findByText(expectedHeaderTitle)).toBeInTheDocument();
 });
+
+test("Change language to Brazilian Portuguese", async () => {
+  //arrange
+  const enMessages = await import("../../compiled-lang/en.json");
+  const ptbrMessages = await import("../../compiled-lang/pt-br.json");
+
+  const initialHeaderTitle = enMessages["app-title"][0].value;
+  const expectedHeaderTitle = ptbrMessages["app-title"][0].value;
+
+  render(<App />);
+
+  expect(screen.getByText(initialHeaderTitle)).toBeInTheDocument();
+
+  //act
+  const ptLanguageButton = screen.getByRole("button", {
+    name: 'Pt-Br',
+  });
+
+  userEvent.click(ptLanguageButton);
+
+  //assert
+  expect(await screen.findByText(expectedHeaderTitle)).toBeInTheDocument();
+});
+
+test("Change language to Portuguese and then to English", async () => {
+  //arrange
+  const enMessages = await import("../../compiled-lang/en.json");
+  const ptbrMessages = await import("../../compiled-lang/pt-br.json");
+
+  const enHeaderTitle = enMessages["app-title"][0].value;
+  const ptbrHeaderTitle = ptbrMessages["app-title"][0].value;
+
+  render(<App />);
+
+  expect(screen.getByText(enHeaderTitle)).toBeInTheDocument();
+
+  const ptLanguageButton = screen.getByRole("button", {
+    name: 'Pt-Br',
+  });
+
+  userEvent.click(ptLanguageButton);
+
+  expect(await screen.findByText(ptbrHeaderTitle)).toBeInTheDocument();
+  //act
+
+  const enLanguageButton = screen.getByRole("button", {
+    name: 'En',
+  });
+
+  userEvent.click(enLanguageButton);
+
+  //assert
+  expect(await screen.findByText(enHeaderTitle)).toBeInTheDocument();
+});
