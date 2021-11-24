f3e585a8964f9190b819332568263d66400adc6c
Daniel Garcia <dlgarciabr@gmail.com>
2021-11-24 11:26:15 +0000

    added a basic util test

--- src/components/Dummy/index.tsx
+++ src/components/Dummy/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Props } from '../../global';
 
 const Dummy: React.FC<Props> = () => {
-    console.log("render memo")
     return <p>dummy comp</p>
 }
 
--- src/utils/i18n/test.ts
+++ src/utils/i18n/test.ts
@@ -1,3 +1,24 @@
-import { loadLocaleMessages } from './index';
+import { getUserLanguage, loadLocaleMessages } from './index';
 
-test.todo("implement i18n tests");
+test("Recover user language from browser", () => {
+    //arrange
+    const expectedLanguage = "en";
+
+    //act
+    const userLanguage = getUserLanguage();
+
+    //assert
+    expect(userLanguage).toBe(expectedLanguage);
+});
+
+test("Recover Language message for Brazilian Portuguese", async () => {
+    //arrange
+    const locale = "pt-br";
+    const ptbrFileMessages = await import("../../compiled-lang/pt-br.json");
+
+    //act
+    const messages = loadLocaleMessages(locale);
+
+    //assert
+    expect(messages["app-title"][0].value).toBe(ptbrFileMessages["app-title"][0].value);
+});
