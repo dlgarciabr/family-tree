02c1353a35b990e8d858098da1987d368f420f38
Daniel Garcia <dlgarciabr@gmail.com>
2021-11-30 12:17:44 +0000

    fixed ESLint problems

--- src/utils/errorMiddleware.ts
+++ src/utils/errorMiddleware.ts
@@ -3,19 +3,23 @@ import {
   Middleware,
   isRejected
 } from '@reduxjs/toolkit';
-import { AnyAction } from 'redux';
+import { AnyAction, Dispatch } from 'redux';
 
 import { dispatchErrorNotification } from '../hooks/notificationHandler';
 
-export const rtkQueryErrorLogger: Middleware = (api: MiddlewareAPI) => (dispatch) =>
-  (action: AnyAction) => {
-    if (isRejected(action)) {
-      if ((action.payload as any).status === 404) {
-        dispatchErrorNotification(dispatch, 'try again later');
-      } else {
-        dispatchErrorNotification(dispatch, 'error not handled');
-      }
+const handler = (action: AnyAction, dispatch: Dispatch) => {
+  if (isRejected(action)) {
+    if ((action.payload as any).status === 404) {
+      dispatchErrorNotification(dispatch, 'try again later');
+      return;
     }
+    dispatchErrorNotification(dispatch, 'error not handled');
+  }
+};
 
+export const rtkQueryErrorLogger: Middleware = (api: MiddlewareAPI) => (dispatch) => (
+  (action: AnyAction) => {
+    handler(action, dispatch);
     return dispatch(action);
-  };
+  }
+);
