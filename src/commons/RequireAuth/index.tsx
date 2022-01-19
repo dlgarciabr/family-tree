1a3e832c9f028bfe6795ce526f617216359b3349
Daniel Garcia <dlgarciabr@gmail.com>
2022-01-19 18:48:41 +0000

    fixed ESLint problems

--- src/commons/RequireAuth/index.tsx
+++ src/commons/RequireAuth/index.tsx
@@ -1,7 +1,7 @@
 import React, { memo, useEffect } from 'react';
 import { useLocation, Navigate, useNavigate } from 'react-router-dom';
 
-import { AuthenticationContext } from 'context/Authentication';
+import { AuthenticationContext, actions } from 'context/Authentication';
 import { useLazyValidateTokenQuery } from 'services/familyTreeApi';
 
 interface Props {
@@ -10,7 +10,7 @@ interface Props {
 
 const RequireAuth: React.FC<Props> = ({ children }) => {
   const [checkTokenValidity] = useLazyValidateTokenQuery();
-  const storageCredentials = sessionStorage.getItem('credentials');
+  const sessionStorageCredentials = sessionStorage.getItem('credentials');
   const { settings: { user }, dispatch } = React.useContext(AuthenticationContext);
   const location = useLocation();
   const navigate = useNavigate();
@@ -23,24 +23,24 @@ const RequireAuth: React.FC<Props> = ({ children }) => {
     const payload = await checkTokenValidity({ token: credentials.token });
 
     if (payload.data && payload.data.valid) {
-      dispatch({ type: "USER_LOGGEDIN", user: { ...credentials } });
+      dispatch({ type: actions.USER_LOGGEDIN, user: { ...credentials } });
       navigate(nextLocation);
     } else {
-      dispatch({ type: "USER_LOGGEDIN", user: null });
+      dispatch({ type: actions.USER_LOGGEDIN, user: null });
       sessionStorage.clear();
       navigate('/login');
     }
   };
 
   useEffect(() => {
-    if (!user && storageCredentials) {
+    if (!user && sessionStorageCredentials) {
       (async () => {
-        validateToken(storageCredentials, location.pathname);
+        validateToken(sessionStorageCredentials, location.pathname);
       })();
     }
   }, []);
 
-  if (!user && !storageCredentials) {
+  if (!user && !sessionStorageCredentials) {
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
@@ -49,6 +49,6 @@ const RequireAuth: React.FC<Props> = ({ children }) => {
   }
 
   return children;
-}
+};
 
 export default memo(RequireAuth);
--- src/context/Authentication/index.tsx
+++ src/context/Authentication/index.tsx
@@ -5,8 +5,12 @@ import React, {
 } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Props, AuthCredentials, AuthContextType, AuthenticationSettings } from '../../types';
-import { useLoginMutation, useLazyValidateTokenQuery } from '../../services/familyTreeApi';
+import {
+  Props, AuthCredentials, AuthContextType, AuthenticationSettings
+} from '../../types';
+import {
+  useLoginMutation, useLazyValidateTokenQuery
+} from '../../services/familyTreeApi';
 
 export const AuthenticationContext = createContext<AuthContextType>({} as AuthContextType);
 AuthenticationContext.displayName = 'AuthenticationContext';
@@ -21,19 +25,19 @@ const AuthenticationProvider: React.FC<Props> = ({ children }) => {
 
   const navigate = useNavigate();
 
-  const signin = async (credentials: AuthCredentials, callback: VoidFunction) => {
-    try {
-      fetchToken({ userLoginData: credentials })
-        .then((payload) => {
-          dispatch({ type: actions.USER_LOGGEDIN, user: { ...payload } });
-          if (callback) {
-            callback();
-          }
-        });
-    } catch (error) {
-      // handled by error middleware
-    }
-  };
+  // const signin = async (credentials: AuthCredentials, callback: VoidFunction) => {
+  //   try {
+  //     fetchToken({ userLoginData: credentials })
+  //       .then((payload) => {
+  //         dispatch({ type: actions.USER_LOGGEDIN, user: { ...payload } });
+  //         if (callback) {
+  //           callback();
+  //         }
+  //       });
+  //   } catch (error) {
+  //     // handled by error middleware
+  //   }
+  // };
 
   // TODO
   const signout = (callback: VoidFunction) => {
@@ -41,29 +45,29 @@ const AuthenticationProvider: React.FC<Props> = ({ children }) => {
     callback();
   };
 
-  const validateToken = async (storageCredentials: string, nextLocation: string) => {
-    if (settings.user) {
-      return;
-    }
-    const credentials = JSON.parse(storageCredentials);
-    const payload = await checkTokenValidity({ token: credentials.token });
+  // const validateToken = async (storageCredentials: string, nextLocation: string) => {
+  //   if (settings.user) {
+  //     return;
+  //   }
+  //   const credentials = JSON.parse(storageCredentials);
+  //   const payload = await checkTokenValidity({ token: credentials.token });
 
-    if (payload.data && payload.data.valid) {
-      dispatch({ type: actions.USER_LOGGEDIN, user: { ...credentials } });
-      navigate(nextLocation);
-    } else {
-      dispatch({ type: actions.USER_LOGGEDIN, user: null });
-      sessionStorage.clear();
-      navigate('/login');
-    }
-  };
+  //   if (payload.data && payload.data.valid) {
+  //     dispatch({ type: actions.USER_LOGGEDIN, user: { ...credentials } });
+  //     navigate(nextLocation);
+  //   } else {
+  //     dispatch({ type: actions.USER_LOGGEDIN, user: null });
+  //     sessionStorage.clear();
+  //     navigate('/login');
+  //   }
+  // };
 
-  const initialAuthenticationSettings: AuthenticationSettings = {
-    signin,
-    signout,
-    validateToken,
-    user: null
-  };
+  // const initialAuthenticationSettings: AuthenticationSettings = {
+  //   signin,
+  //   signout,
+  //   validateToken,
+  //   user: null
+  // };
 
   const [settings, dispatch] = useReducer((state: AuthenticationSettings, action: any) => {
     switch (action.type) {
@@ -78,15 +82,44 @@ const AuthenticationProvider: React.FC<Props> = ({ children }) => {
         throw new Error();
     }
   }, {
-    ...initialAuthenticationSettings
+    signin: async (credentials: AuthCredentials, callback: VoidFunction) => {
+      try {
+        fetchToken({ userLoginData: credentials })
+          .then((payload) => {
+            dispatch({ type: actions.USER_LOGGEDIN, user: { ...payload } });
+            if (callback) {
+              callback();
+            }
+          });
+      } catch (error) {
+        // handled by error middleware
+      }
+    },
+    signout,
+    validateToken: async (storageCredentials: string, nextLocation: string) => {
+      if (settings.user) {
+        return;
+      }
+      const credentials = JSON.parse(storageCredentials);
+      const payload = await checkTokenValidity({ token: credentials.token });
+
+      if (payload.data && payload.data.valid) {
+        dispatch({ type: actions.USER_LOGGEDIN, user: { ...credentials } });
+        navigate(nextLocation);
+      } else {
+        dispatch({ type: actions.USER_LOGGEDIN, user: null });
+        sessionStorage.clear();
+        navigate('/login');
+      }
+    },
+    user: null
   });
 
   // const providerProps = useMemo(() => ({
-  //   user, setUser, signin, signout, validateToken
+  //   settings, dispatch
   // }), []);
 
   return (
-    // <AuthenticationContext.Provider value={{ user, setUser: setContextUser, signin, signout, validateToken }}>
     // <AuthenticationContext.Provider value={providerProps}>
     <AuthenticationContext.Provider value={{ settings, dispatch }}>
       {children}
